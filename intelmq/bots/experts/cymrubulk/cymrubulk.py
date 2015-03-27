6d7e1b81fc88266ae106a4e34c283e98ba66a5ee
zurgutt <zurgutt@localhost>
2015-03-27 14:31:58 +0200

    initial unrun version of cymrubulk, needs debug

--- intelmq/bots/experts/cymrubulk/cymrubulk.py
+++ intelmq/bots/experts/cymrubulk/cymrubulk.py
@@ -4,7 +4,7 @@ from intelmq.lib.cache import Cache
 from intelmq.bots import utils
 from intelmq.bots.experts.cymrubulk.lib import CymruBulk
 
-from cymru.ip2asn import dns
+from cymru.ip2asn.whois import WhoisClient as ip2asnWhois
 
 MINIMUM_BGP_PREFIX_IPV4 = 24
 MINIMUM_BGP_PREFIX_IPV6 = 128 # FIXME
@@ -21,81 +21,55 @@ class CymruBulkExpertBot(Bot):
 
     
     def process(self):
-        for chunk_count in xrange(1, self.parameters.maxchunk):
-            event = self.receive_message()
-            
-            if not event:
-                break
-            
-            events.append(event)
+        events = self.reserve(self.parameters.maxchunk)
         
-        if not events:
-            self.acknowledge_message()
-            return
-
-        keys = ["source_%s", "destination_%s"]
+        self.ipList = [] 
+        for event in events:
+            keys = ["source_%s", "destination_%s"]
+            for key in keys:
+                ip = event.value(key % "ip")
+                self.ipList.append(ip)
         
-        # Make a bulk request
+        # Do the Cymru query, get dictionary of results.
+        ipSet = set(ipList)
+        cymruClient = ip2asnWhois()
+        cymruResult = cymruClient.lookupmany(ipSet, qType)
         
-        for key in keys:
-            ip = event.value(key % "ip")
+        for event in events:
+            keys = ["source_%s", "destination_%s"]
+            for key in keys:
+                ip = event.value(key % "ip")
+                if not cymruResult[ip]:
+                    continue
+                
+                result = cymruResult[ip]
             
-            if not ip:
-                self.send_message(event)
-                self.acknowledge_message()
-                return
+                if "asn" in result:
+                    event.clear(key % 'asn')
+                    event.add(key % 'asn',        result['asn'])
+                    
+                if "prefix" in result:
+                    event.clear(key % 'bgp_prefix')
+                    event.add(key % 'bgp_prefix', result['bgp_prefix'])
+                    
+                if "lir" in result:
+                    event.clear(key % 'registry')
+                    event.add(key % 'registry',   result['registry'])
+                    
+                if "date" in result:
+                    event.clear(key % 'allocated')
+                    event.add(key % 'allocated',  result['allocated'])
+                    
+                if "owner" in result:
+                    event.clear(key % 'as_name')
+                    event.add(key % 'as_name',    result['as_name'])
+                    
+                if "cc" in result:
+                    event.clear(key % 'cymrubulk_cc')
+                    event.add(key % 'cymrubulk_cc',   result['cc'])
 
-            elif utils.is_ipv4(ip):
-                ip_version = 4
-                ip_integer = utils.ip_to_int(ip)
-                cache_key = bin(ip_integer)[2 : MINIMUM_BGP_PREFIX_IPV4 + 2]
-
-            elif utils.is_ipv6(ip):
-                ip_version = 6
-                ip_integer = utils.ip_to_int(ip)
-                cache_key = bin(ip_integer)[2 : MINIMUM_BGP_PREFIX_IPV6 + 2]
-
-            else:
-                self.send_message(event)
-                self.acknowledge_message()
-                return
-
-
-            result_json = self.cache.get(cache_key)
-
-            if result_json:
-                result = json.loads(result_json)
-            else:
-                result = CymruBulk.query(ip, ip_version)
-                result_json = json.dumps(result)
-                self.cache.set(cache_key, result_json)
-            
-            if "asn" in result:
-                event.clear(key % 'asn')
-                event.add(key % 'asn',        result['asn'])
-                
-            if "bgp_prefix" in result:
-                event.clear(key % 'bgp_prefix')
-                event.add(key % 'bgp_prefix', result['bgp_prefix'])
-                
-            if "registry" in result:
-                event.clear(key % 'registry')
-                event.add(key % 'registry',   result['registry'])
-                
-            if "allocated" in result:
-                event.clear(key % 'allocated')
-                event.add(key % 'allocated',  result['allocated'])
-                
-            if "as_name" in result:
-                event.clear(key % 'as_name')
-                event.add(key % 'as_name',    result['as_name'])
-                
-            if "cc" in result:
-                event.clear(key % 'cymrubulk_cc')
-                event.add(key % 'cymrubulk_cc',   result['cc'])
-
-        self.send_message(event)
-        self.acknowledge_message()
+        # TODO: Should have more granularity, error management here.
+        self.release()
         
 if __name__ == "__main__":
     bot = CymruBulkExpertBot(sys.argv[1])
